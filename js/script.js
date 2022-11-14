79961c2863e4dde832c40facc12735ff62cb0603
kormaks <115947542+kormaks@users.noreply.github.com>
2022-11-14 09:34:41 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,53 @@
+$(".slider__wrapper").bxSlider({
+    pager: true,
+    controls: false,
+    infiniteLoop: true,
+    touchEnable: true,
+    hideControlOnEnd: false,
+
+});
+// const element = document.querySelector(".features_info");
+// let target = EventTarget;
+// console.log(target);
+// const elementThree = document.querySelector("#features-search__three");
+// const elementTwo = document.querySelector("#features-search__two");
+// let count = 0;
+// let countTwo = 0;
+// let countThree = 0;
+// const newText = this.document.createElement("p");
+// newText.classList.add("add_text");
+// newText.innerText = "1";
+// const newTextTwo = this.document.createElement("p");
+// newTextTwo.classList.add("add_text");
+// newTextTwo.innerText = "2";
+// const newTextThree = this.document.createElement("p");
+// newTextThree.classList.add("add_text");
+// newTextThree.innerText = "3";
+// addEventListener("click", function () {
+//     if(count === 0){
+//         element.append(newText);
+//         return count = count + 1;
+//     }else{
+//         newText.remove();
+//         return count = count - 1;
+//     }
+// });
+// addEventListener("click", function () {
+//     if(countTwo === 0){
+//         elementTwo.append(newTextTwo);
+//         return countTwo = countTwo + 1;
+//     }else{
+//         newTextTwo.remove();
+//         return countTwo = countTwo - 1;
+//     }
+// });
+// addEventListener("click", function () {
+//     if(countThree === 0){
+//         elementThree.append(newTextThree);
+//         return countThree = countThree + 1;
+//     }else{
+//         newTextThree.remove();
+//         return countThree = countThree - 1;
+//     }
+// });
+// console.log(count , countTwo, countThree)
